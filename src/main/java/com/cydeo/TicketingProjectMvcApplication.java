30cacf8c127d00849dbe484a525b7f86a6466ced
MertRam <mert.ramadan35@gmail.com>
2023-08-22 19:03:27 +0300

    first classes added

--- src/main/java/com/cydeo/TicketingProjectMvcApplication.java
+++ src/main/java/com/cydeo/TicketingProjectMvcApplication.java
@@ -8,6 +8,9 @@ public class TicketingProjectMvcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectMvcApplication.class, args);
+
+        char ch = 'a';
+        System.out.println(ch + 0);
     }
 
 }
