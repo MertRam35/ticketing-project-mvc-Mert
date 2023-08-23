62e8c3afc8dedbd682ee135a1f2191dbea6f301f
MertRam <mert.ramadan35@gmail.com>
2023-08-23 17:56:14 +0300

    user list thymeleaf and data added

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +14,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
 
-@GetMapping("/create")
-    public String createUser(Model model){
-    model.addAttribute("user",new UserDTO());
-    model.addAttribute("users",model);
+    private final RoleService roleService;
+    private final UserService userService;
 
-    return "user/create";
-}
+    @Autowired
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/create")
+    public String createUser(Model model) {
+
+        model.addAttribute("user", new UserDTO());
+
+        model.addAttribute("roles", roleService.findAll());
+
+        model.addAttribute("users", userService.findAll());
+
+        return "user/create";
+    }
 
 }
