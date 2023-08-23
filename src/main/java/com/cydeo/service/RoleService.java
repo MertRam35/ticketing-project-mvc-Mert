7c038d355b61169cd733d367cf574feb1979d576
MertRam <mert.ramadan35@gmail.com>
2023-08-23 09:13:34 +0300

    service package added

---
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -0,0 +1,19 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+
+import java.util.List;
+
+public interface RoleService {
+
+
+    RoleDTO save(RoleDTO role);
+    RoleDTO finById(Long id);
+    List<RoleDTO> findAll();
+    void deleteById(Long id);
+
+
+
+
+}
