a7a5514ced12917b124f32d152b07db9dd882397
MertRam <mert.ramadan35@gmail.com>
2023-08-23 19:19:16 +0300

    day2 finished- form-user created and added user list

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -33,6 +35,18 @@ public class UserController {
         model.addAttribute("users", userService.findAll());
 
         return "user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("user") UserDTO user){
+
+
+
+        userService.save(user);
+
+
+
+        return "redirect:user/create";
     }
 
 }
