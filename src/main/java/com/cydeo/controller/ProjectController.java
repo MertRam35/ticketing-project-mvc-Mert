00b78b4295796ae84f3c49023867d74897e077e2
MertRam <mert.ramadan35@gmail.com>
2023-08-29 17:36:10 +0300

    ProjectController.java added

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProjectController {
 
     @GetMapping("/create")
-    public String projectCreate(){
+    public String projectCreate(Model model){
+        model.addAttribute("project",new ProjectDTO());
+
+        model.addAttribute("managers",)
 
         return "/project/create";
     }
